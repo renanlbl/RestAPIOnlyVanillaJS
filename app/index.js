6fb2bfb47257756da51edac6f12500a759049309
Renan Lima <cursosprogramacoes@gmail.com>
2020-01-26 18:59:07 -0300

    Analisa tipo de método enviado para o servidor

--- app/index.js
+++ app/index.js
@@ -16,11 +16,14 @@ var server = http.createServer(function(req, res) {
 	var trimmedPath = path.replace(/^\/+|\/+$/g,'');
 	
 
+	// Get the HTTP Method
+	var method = req.method.toLowerCase();	
+
 	// Send the response
 	res.end('Hello World\n');
 
 	// Log the request path
-	console.log('Request received on path: ' + trimmedPath)
+	console.log('Request received on path: ' + trimmedPath + 'with method: ' +method);
 	
 });
 
