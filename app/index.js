82744f57a00d015fcc644aaed8651a4459c744c3
Renan Lima <cursosprogramacoes@gmail.com>
2020-01-26 17:11:55 -0300

    Analizando novas requisições vindo para o servidor, retornando seu caminho atual

--- app/index.js
+++ app/index.js
@@ -4,10 +4,24 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 //The server should respond to all request with a string
 var server = http.createServer(function(req, res) {
+	// Get the URL and parse it
+	var parsedUrl = url.parse(req.url, true);
+
+	// Get the path from the URL
+	var path = parsedUrl.pathname;
+	var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+	
+
+	// Send the response
 	res.end('Hello World\n');
+
+	// Log the request path
+	console.log('Request received on path: ' + trimmedPath)
+	
 });
 
 
